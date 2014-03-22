8d1b9de1bc69d50ed42e2ea27719186bef1e8cd9
Vitor Shade <vitor.shade@gmail.com>
2014-03-21 23:55:51 -0300

    -Started modified raytracer to use scene information

--- scene.cpp
+++ scene.cpp
@@ -1,8 +1,17 @@
 #include "scene.h"
 
-bool Triangle::CheckCollision(const Ray &ray)
+#include "../shade-framework/model.h"
+
+bool Triangle::CheckCollision(Ray &ray)
 {
-    return false;
+    float t = 0;
+    ray.collision_t_ = t;
+    ray.collision_point_ = ray.origin_ + (ray.direction_*t);
+    ray.collided_ = true;
+//    ray.collision_normal_ = ray.collision_point_ - this->position_;
+//    ray.collision_normal_.Normalize();
+    ray.mat_ptr_ = new Material();
+    return true;
 }
 
 Scene::Scene()
@@ -14,6 +23,32 @@ Scene::Scene()
 
 }
 
+Scene::Scene(std::string scene_path)
+{
+    Model m;
+    m.Load(scene_path);
+    for(int i = 0; i < m.vertices_index_.size(); i+= 3)
+    {
+        Triangle *t = new Triangle();
+        t->a.x_ = m.vertices_[m.vertices_index_[i]].x;
+        t->a.y_ = m.vertices_[m.vertices_index_[i]].y;
+        t->a.z_ = m.vertices_[m.vertices_index_[i]].z;
+
+        t->b.x_ = m.vertices_[m.vertices_index_[i+1]].x;
+        t->b.y_ = m.vertices_[m.vertices_index_[i+1]].y;
+        t->b.z_ = m.vertices_[m.vertices_index_[i+1]].z;
+
+        t->c.x_ = m.vertices_[m.vertices_index_[i+2]].x;
+        t->c.y_ = m.vertices_[m.vertices_index_[i+2]].y;
+        t->c.z_ = m.vertices_[m.vertices_index_[i+2]].z;
+
+        Vec3 ba = t->b - t->a;
+        Vec3 ca = t->c - t->a;
+        t->normal = ba.Cross(ca);
+        triangles_.push_back(t);
+    }
+}
+
 void Scene::TestSphereCollision(Ray &ray)
 {
     std::vector<Object*>::iterator it;
@@ -22,14 +57,14 @@ void Scene::TestSphereCollision(Ray &ray)
     }
 }
 
-std::vector<Triangle*> Scene::CheckRayCollision(const Ray &ray)
+std::vector<Triangle*> Scene::CheckRayCollision(Ray &ray)
 {
     std::vector<Triangle*> collided_triangles;
-    for(std::vector<Triangle>::iterator it = triangles_.begin();
+    for(std::vector<Triangle*>::iterator it = triangles_.begin();
         it != triangles_.end();
         it++){
-        if((*it).CheckCollision(ray)){
-            collided_triangles.push_back(&(*it));
+        if((*it)->CheckCollision(ray)){
+            collided_triangles.push_back(*it);
         }
     }
     return collided_triangles;
--- scene.h
+++ scene.h
@@ -1,10 +1,10 @@
 #ifndef SCENE_H
 #define SCENE_H
 
-#include "object.h"
-
 #include <vector>
+#include "../shade-framework/model.h"
 
+#include "object.h"
 #include "vec3.h"
 #include "point3.h"
 #include "ray.h"
@@ -17,19 +17,22 @@ public:
     Point3 a;
     Point3 b;
     Point3 c;
-    bool CheckCollision(const Ray&);
+    Vec3 normal;
+    bool CheckCollision(Ray&);
 };
 
 class Scene
 {
 public:
     Scene();
+    Scene(std::string scene_path);
     void TestSphereCollision(Ray& );
-    std::vector<Triangle*> CheckRayCollision(const Ray&);   
-    std::vector<Triangle> triangles_;
+    std::vector<Triangle*> CheckRayCollision(Ray&);
+    std::vector<Triangle*> triangles_;
     PointLight light_;
     std::vector<Object*> objects_;
     Camera camera_;
+    Model scene_model_;
 };
 
 #endif // SCENE_H
